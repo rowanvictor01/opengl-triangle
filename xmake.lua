4760a1f60bc682d52d2da8f024979a138d8ea528
Rowan Victor <209355544+rowanvictor01@users.noreply.github.com>
2026-04-14 18:20:03 +0800

    Setup xmake project config

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,23 @@
-add_rules("mode.debug", "mode.release")
+add_requires("libsdl2", "glad", {
+			configs = {
+			    spec = "gl",
+			    version = "4.6",
+			    profile = "core",
+			    extensions = "true",
+			    loader = true
+			}
+})
+
+set_toolchains("gcc-15")
+set_languages("cxx23")
+add_cxxflags("-pedantic-errors", "-Wall", "-Weffc++", "-Wextra", "-Wconversion", "-Wsign-conversion", "-Werror", "-g")
+
 
 target("opengl-triangle")
     set_kind("binary")
     add_files("src/*.cpp")
+    add_packages("libsdl2", "glad")
+
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
